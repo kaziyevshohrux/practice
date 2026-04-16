784a6a9f66a0ed27cbe77864494d2632d5d025e0
OMAR <OMAR@comets.com>
2026-04-17 01:25:42 +0900

    feat : learn function which is define and call steps

---
+++ function.py
@@ -0,0 +1,38 @@
+'''
+1) define , call 
+2) parametr , argument 
+3) keyword , default 
+4) scope
+
+'''
+
+# function ; block of code that performs a specific task
+
+# java {}   , python : indentation
+
+# define
+
+
+
+
+
+def greet(name):
+    # pass  # for empty function
+    print(f"Hello, {name}!")
+def greeting(b):
+    print('greeting is executed')
+    return f"Hio, {b}!"
+
+# call
+result = greet('OMAR')
+
+print(result)
+
+result2 = greeting('shokhrux')
+print(result2)
+
+
+# parametr   , argument
+
+# parametr = define qilganda beriladigan nom
+# argument = call qilganda beriladigan qiymat
