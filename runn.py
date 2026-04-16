665f57345f283541cb09adaf0b90f44afa9452a4
OMAR <OMAR@comets.com>
2026-04-17 00:38:15 +0900

    feat: python verables and dunder

--- runn.py
+++ runn.py
@@ -1,4 +1,17 @@
+# Dunder __builtins__ , __init__
+
 a = 100 
 print(a)
 
-print('heloo')
+print('hello')
+
+''''
+ 
+in python , is theree= are bultin tools:
+
+1) FUNCTION:  print()  # for printing the output ,  input()  # for taking input from user  len () , type()
+2) Type : int , str , list , dict , set , tuple  # for defining the type of data
+3) CONSTANT : True , False , None  # for defining the constant values
+'''
+
+print(dir(__builtins__))  # for printing the list of built-in functions and types
