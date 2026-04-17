50d2c0ffe7edd198ebb393d041ad78fb7760e502
OMAR <OMAR@comets.com>
2026-04-17 18:11:50 +0900

    feat : train MIT

--- train.js
+++ train.js
@@ -1,3 +1,11 @@
+/*   A-task 
+
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+
+
+//Masala yechimi 
 function countletter(harf, str){
     let count = 0 
    let a = str.split('')
