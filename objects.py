4182eb2bf2ed9f919a08cea21cbb46ebb2204345
OMAR <OMAR@comets.com>
2026-04-18 14:34:32 +0900

    feat: learn python object

---
+++ objects.py
@@ -0,0 +1,15 @@
+# objects -> maxsus proprtyga ega bolgan data type 
+import array 
+import math
+
+from math import ceil, asin
+print('============')
+
+print(type('hello world'))
+print(type(123))
+print(type(3.14))
+print(type(True))
+print(type(math))
+print(type(array))
+
+print(ceil(98.1))
