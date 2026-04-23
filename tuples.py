f10cdde377ef5401b7ed35f00e9778b457c889a5
OMAR <OMAR@comets.com>
2026-04-23 12:24:31 +0900

    feat: tuples and list

---
+++ tuples.py
@@ -0,0 +1,27 @@
+'''
+1)tuples
+
+'''
+
+num = [1,2,3,4,5,6]
+l = ['helllllllloouuuuuuuu']
+obj_c = dict(name='umar', year=2000)
+
+print(num)
+print(l)
+print(obj_c)
+
+fruits= ['banan', 'olma', 'nok']
+fruits[2]= 'behi'
+fruits.append('nok')
+print(fruits)
+
+print('=======tuple=======')
+
+fruits = ('papaya', 'mango', 'kakos')
+print(fruits)
+
+#fruits[0]='00'
+#fruits.append('465') 
+
+print(fruits)
