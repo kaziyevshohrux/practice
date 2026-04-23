cba61bc17f2cd55b333df07772c922cdb5dd06cd
OMAR <OMAR@comets.com>
2026-04-23 16:41:55 +0900

    feat: learn list and list methods

---
+++ lists.py
@@ -0,0 +1,72 @@
+'''
+list()
+methods()
+lumbda function()
+enamurate , map , filter
+
+'''
+
+person = dict(name='umar', age = 22)    #dictionary
+mix = (10 , 'salom' , False)            #tuple
+grups = ['mit41', 'mit43', 'mit39']
+
+for i in grups:
+    print('grups name' , i)
+
+
+fruits = ['apple', 'abrikos', 'mango', 'hurmo']
+
+a= fruits[2]
+b= fruits[0:2]
+c= fruits[2:3]
+d=fruits[1:3:1]
+f=fruits[0::3]
+j=fruits[::-1]
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(f)
+print(j)
+
+print('=========================methods=============================')
+# list methods  ->   append(), insert(), pop(), remove(), clear(), sort(), sorted(), index(), 
+
+
+animal = ['dog', 'fish', 'cat', 'lion', 'tiger']
+print(animal)
+
+animal.append('mice')
+print(animal)
+
+animal.insert(0,'chubakabra')
+print(animal)
+
+size = len(animal)-1
+animal.pop(size)
+print(animal)
+
+animal.pop(0)
+print(animal)
+
+animal.remove('fish')
+print(animal)
+
+a=animal.index('lion')
+print(a)
+
+animal.clear()
+print(animal)
+
+
+numbers = [2,5,6,22,2,45,2,23,12,5,0,202]
+
+numbers.sort()
+
+print(numbers)
+
+sortednum = sorted(numbers)
+
+print(sortednum)
