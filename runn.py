4374e94045bf3433d53048d7973532ce2ab90a73
OMAR <OMAR@comets.com>
2026-04-25 14:46:12 +0900

    faet: dunder -> builtins

--- runn.py
+++ runn.py
@@ -11,7 +11,7 @@ in python , is theree= are bultin tools:
 
 1) FUNCTION:  print()  # for printing the output ,  input()  # for taking input from user  len () , type()
 2) Type : int , str , list , dict , set , tuple  # for defining the type of data
-3) CONSTANT : True , False , None  # for defining the constant values
+3) CONSTANT : True , False , Nne  # for defining the constant values
 '''
 
 print(dir(__builtins__))  # for printing the list of built-in functions and types
