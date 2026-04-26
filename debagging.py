adc4dc94e8461951b5fcef957d00f793aba9186c
OMAR <OMAR@comets.com>
2026-04-26 16:40:48 +0900

    feat: pip , packed manager

--- debagging.py
+++ debagging.py
@@ -29,9 +29,27 @@ finally:
     my_file.close()
 
 
+
 # with context manager hisoblanadi 
 
 with open("material/massage.txt", "r") as your_file:
     your_file = your_file.read()
     print(your_file)
 print('done')
+
+
+print('=====external packages=======')
+
+# core packages pytondi ozi bilan keladi 
+
+# external packages ladi install qilishimiz kerak boladi 
+
+# external   ->    https://pypi.org/ 
+
+
+from PIL import Image
+
+with Image.open("material/egpt.jpg") as img_obj:
+   resize_img= img_obj.resize((200,200))
+   resize_img.show()
+   resize_img.save("material/sample.jpg")
