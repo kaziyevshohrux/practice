42ab8cebb6f54ca0344d423183b05948a18db513
OMAR <OMAR@comets.com>
2026-05-14 14:33:12 +0900

    feat: M-task python

--- train.py
+++ train.py
@@ -1,18 +1,49 @@
-# K-TASK (PYTHON)
+# M-TASK (PYTHON)
 
-# Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
-# MASALAN: find_longest("I come from Uzbekistan") return "Uzbekistan
-import math 
-def find_longest(gap):
-    a=gap.split()
-    last=[]
-    for i in a:
-        last.append(len(i))
-    s = max(last)
-    d=last.index(s)
-    print(a[d])
+# Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+# MASALAN: palindrom_check("dad") return True;  palindrom_check("son") return False;
 
-find_longest("I comeeeeeeeeeeeee from Uzbekistanuzbekistan salom")
+
+def palindrom(gap):
+    for i in gap[-1::-1]:
+        break
+    for j in gap:
+        break
+
+    if i==j:
+        print(True)
+    else:
+        print(False)
+palindrom('adada')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # K-TASK (PYTHON)
+
+# # Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin.
+# # MASALAN: find_longest("I come from Uzbekistan") return "Uzbekistan
+# import math 
+# def find_longest(gap):
+#     a=gap.split()
+#     last=[]
+#     for i in a:
+#         last.append(len(i))
+#     s = max(last)
+#     d=last.index(s)
+#     print(a[d])
+
+# find_longest("I comeeeeeeeeeeeee from Uzbekistanuzbekistan salom")
 
 
 
